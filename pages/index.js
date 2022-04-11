62e41feec83436ab0b6692d55e1ab9f6e35146ff
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2022-04-11 18:41:09 +0300

    Fetch data from graphqlAPI

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {PostCard,PostWidget,Categories} from '../components/'
 
-const posts=[
-  {title:'React Testing',except:'Learn React Testing'},
-  {title:'React with Tailwind',except:'Learn React with Tailwind'}
-];
+import{getPosts} from '../services';
 
-export default function Home() {
+// const posts=[
+//   {title:'React Testing',except:'Learn React Testing'},
+//   {title:'React with Tailwind',except:'Learn React with Tailwind'}
+// ];
+
+export default function Home({posts}) {
   return (
     <div className="conatiner mx-auto px-10 mb-8 ">
       <Head>
@@ -19,18 +21,27 @@ export default function Home() {
 
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
       <div className="lg:col-span-8 col-span-1">
-      {posts.map((post,index) =>(
-        <PostCard/>
+      {posts.map((post) =>(
+        <PostCard post={post} key={post.title}/>
       ))}
       </div>
     </div>
 
     <div className="lg:col-span-4 col-span-1">
         <div className="lg:sticky relative top-8">
-
+          <PostWidget />
+          <Categories />
         </div>
     </div>
      
     </div>
   )
+}
+
+export async function getStaticProps(){
+  const posts=(await getPosts())||[];
+
+  return {
+    props:{props}
+  }
 }
