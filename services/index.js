8e67e1b0a9d9fb8f084f914a79068a6b014ed022
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2022-04-12 13:06:13 +0300

    tried the fix

--- services/index.js
+++ services/index.js
@@ -1,4 +1,4 @@
-import { request, gql } from "graphql-request";
+import { request, gql } from 'graphql-request';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
@@ -7,11 +7,12 @@ export const getPosts = async () => {
     query MyQuery {
       postsConnection {
         edges {
+          cursor
           node {
             author {
               bio
-              id
               name
+              id
               photo {
                 url
               }
